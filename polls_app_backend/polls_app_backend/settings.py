516027b288675e327ae2d56c6285e08ea26c55cb
giftty <wwwgift@yahoo.com>
2024-06-15 17:40:01 +0100

    added zones

--- polls_app_backend/polls_app_backend/settings.py
+++ polls_app_backend/polls_app_backend/settings.py
@@ -87,14 +87,20 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
-     "default": {
+    #  "default": {
+    #     "ENGINE": "django.db.backends.mysql",
+    #      "NAME":"polls_app",
+    #       "USER": "root",
+    #       "PASSWORD": "",
+    #       "HOST": "localhost"    
+    # }
+    "default": {
         "ENGINE": "django.db.backends.mysql",
          "NAME":"polls_app",
-          "USER": "root",
-          "PASSWORD": "",
+          "USER": "serveruser",
+          "PASSWORD": "serverdata",
           "HOST": "localhost"    
     }
-
 }
 
 
